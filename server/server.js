fac153c085b7c56e83f4b51f0010d802a5a0a1a1
Kishan <kishansingh88046@gmail.com>
2025-04-05 22:12:52 +0530

    cors options added

--- server/server.js
+++ server/server.js
@@ -8,8 +8,14 @@ import imageRouter from "./routes/imageRoutes.js"
 const PORT  = process.env.PORT || 4000
 const app = express()
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 await connectDB()
 
 app.use("/api/user", userRouter)
